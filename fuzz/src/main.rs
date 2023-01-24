cd46e51efe4d7d4126aa68b206996c818fff901f
Lilith Silvestris <120892488+oftheforest@users.noreply.github.com>
2023-01-23 17:06:04 -0800

    Add preliminary fuzzing support (afl.rs)
    afl.rs requires an input directory with files to be used as starting points; a
    variety of valid mp4 files should probably be collected and committed for that
    purpose.

---
+++ fuzz/src/main.rs
@@ -0,0 +1,11 @@
+use std::io::Cursor;
+
+use afl::fuzz;
+use mp4::Mp4Reader;
+
+fn main() {
+    fuzz!(|bytes: &[u8]| {
+        let reader = Cursor::new(bytes);
+        let _ = Mp4Reader::read_header(reader, bytes.len().try_into().unwrap());
+    });
+}
